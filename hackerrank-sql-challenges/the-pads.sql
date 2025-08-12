026e7d48e273174f51aced75f0017c616e44c2fd
Alyssa Llanes <41835506+alyllanes@users.noreply.github.com>
2025-08-12 18:39:32 +0800

    Create the-pads.sql

---
+++ hackerrank-sql-challenges/the-pads.sql
@@ -0,0 +1,13 @@
+-- https://www.hackerrank.com/challenges/the-pads/problem
+select
+    concat(name, '(', upper(left(occupation, 1)), ')')
+from occupations
+order by name;
+
+select concat('There are a total of ', occupation_count, ' ', occupation, 's.') from
+(select
+    lower(occupation) as occupation,
+    count(Name) as occupation_count
+from occupations
+group by 1) a
+order by occupation_count, occupation;

788c8c6dfa820d5691e486e02a431e30e9f6092a
Alyssa Llanes <41835506+alyllanes@users.noreply.github.com>
2025-08-12 22:44:45 +0800

    Create removing_duplicates.sql

---
+++ bigquery-exercises/removing_duplicates.sql
@@ -0,0 +1,9 @@
+-- Dataset: bigquery-public-data.cms_medicare.inpatient_charges_2011
+-- 💡 Prompt:
+-- Identify and remove duplicate records based on:
+--  provider_id, drg_definition, total_discharges.
+
+select * from `bigquery-public-data.cms_medicare.inpatient_charges_2011` limit 10;
+
+select * from `bigquery-public-data.cms_medicare.inpatient_charges_2011`
+qualify 1=row_number() over (partition by provider_id, drg_definition, total_discharges order by average_total_payments desc);

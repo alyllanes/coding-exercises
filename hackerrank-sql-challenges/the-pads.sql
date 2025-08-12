5a03ee8e8b86027c179a99c80c150190370c590a
Alyssa Llanes <41835506+alyllanes@users.noreply.github.com>
2025-08-12 18:40:25 +0800

    Update the-pads.sql

--- hackerrank-sql-challenges/the-pads.sql
+++ hackerrank-sql-challenges/the-pads.sql
@@ -4,10 +4,13 @@ select
 from occupations
 order by name;
 
-select concat('There are a total of ', occupation_count, ' ', occupation, 's.') from
-(select
-    lower(occupation) as occupation,
-    count(Name) as occupation_count
-from occupations
-group by 1) a
+select 
+    concat('There are a total of ', occupation_count, ' ', occupation, 's.') 
+from (
+    select
+        lower(occupation) as occupation,
+        count(Name) as occupation_count
+    from occupations
+    group by 1
+    ) a
 order by occupation_count, occupation;
